0ee32bc1f370f1c9f4b05a59ecbda87868240bfb
WHUCScoke <1194018242@qq.com>
2024-07-08 15:46:34 +0800

    充值界面

--- qt_stock_trade_platform/build/Desktop_Qt_6_6_3_MinGW_64_bit-Debug/ui_recharge.h
+++ qt_stock_trade_platform/build/Desktop_Qt_6_6_3_MinGW_64_bit-Debug/ui_recharge.h
@@ -27,14 +27,16 @@ public:
     {
         if (recharge->objectName().isEmpty())
             recharge->setObjectName("recharge");
-        recharge->resize(758, 421);
+        recharge->resize(501, 371);
+        recharge->setStyleSheet(QString::fromUtf8("\n"
+"background-color: rgb(255, 255, 255);"));
         sureButtom = new QPushButton(recharge);
         sureButtom->setObjectName("sureButtom");
-        sureButtom->setGeometry(QRect(290, 230, 80, 18));
+        sureButtom->setGeometry(QRect(170, 210, 80, 18));
         rechargeEdit = new QLineEdit(recharge);
         rechargeEdit->setObjectName("rechargeEdit");
-        rechargeEdit->setGeometry(QRect(220, 150, 289, 22));
-        rechargeEdit->setEchoMode(QLineEdit::Password);
+        rechargeEdit->setGeometry(QRect(80, 160, 289, 22));
+        rechargeEdit->setEchoMode(QLineEdit::Normal);
 
         retranslateUi(recharge);
 
@@ -45,6 +47,8 @@ public:
     {
         recharge->setWindowTitle(QCoreApplication::translate("recharge", "Dialog", nullptr));
         sureButtom->setText(QCoreApplication::translate("recharge", "Sure", nullptr));
+        rechargeEdit->setInputMask(QString());
+        rechargeEdit->setPlaceholderText(QCoreApplication::translate("recharge", "\350\257\267\350\276\223\345\205\245\344\275\240\346\203\263\350\246\201\345\205\205\345\200\274\347\232\204\351\207\221\351\242\235", nullptr));
     } // retranslateUi
 
 };
